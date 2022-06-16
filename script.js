e314644c76c5084abba55a368236cce9a6767c14
Ryan <rygho90@gmail.com>
2022-06-15 21:24:14 -0400

    Add create new project functionality

--- script.js
+++ script.js
@@ -1,14 +1,37 @@
 const projectContainer = document.querySelector('[data-projects]');
+const newProjectForm = document.querySelector('[data-new-project-form]')
+const newProjectInput = document.querySelector('[data-new-project-input]')
 
-const projects = ['Test 1', 'Test 2']
+const projects = [{
+    id: 1,
+    name: 'name'
+}, {
+    id: 2,
+    name: 'todo'
+}]
+
+newProjectForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const projectName = newProjectInput.value;
+    if (projectName == null || projectName === '') return
+    const project = createProject(projectName);
+    newProjectInput.value = null;
+    projects.push(project);
+    render();
+})
+
+function createProject(name) {
+    return { id: Date.now().toString(), name: name, tasks: [] }
+}
 
 function render() {
     clearElement(projectContainer);
     projects.forEach(project => {
-        const projectListItem = document.createElement("li");
-        projectListItem.classList.add("project-name");
-        projectListItem.innerText = project;
-        projectContainer.append(projectListItem);
+        const projectElement = document.createElement("li");
+        projectElement.dataset.listId = project.id;
+        projectElement.classList.add("project-name");
+        projectElement.innerText = project.name;
+        projectContainer.append(projectElement);
     })
 }
 
