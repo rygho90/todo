04703601c1151559653f0d3c3f69fa1f20c6ac8d
Ryan <rygho90@gmail.com>
2022-06-18 14:43:42 -0400

    Show current project titles

--- script.js
+++ script.js
@@ -2,11 +2,14 @@ const projectContainer = document.querySelector('[data-projects]');
 const newProjectForm = document.querySelector('[data-new-project-form]');
 const newProjectInput = document.querySelector('[data-new-project-input]');
 const deleteProjectBtn = document.querySelector('[data-delete-project-btn]');
+const contentContainer = document.querySelector('[data-content-container]');
+const contentTitle = document.querySelector('[data-content-title]');
+const tasksContainer = document.querySelector('[data-tasks]');
 
 const LOCAL_STORAGE_PROJECT_KEY = 'task.projects';
 const LOCAL_STORAGE_SELECTED_PROJECT_ID_KEY = 'task.selectedProjectId';
 let projects = JSON.parse(localStorage.getItem(LOCAL_STORAGE_PROJECT_KEY)) || [];
-let selectedProjectId = localStorage.getItem(LOCAL_STORAGE_SELECTED_PROJECT_ID_KEY)
+let selectedProjectId = localStorage.getItem(LOCAL_STORAGE_SELECTED_PROJECT_ID_KEY) || null;
 
 projectContainer.addEventListener('click', e => {
     if (e.target.tagName.toLowerCase() === 'li') {
@@ -48,6 +51,19 @@ function save() {
 
 function render() {
     clearElement(projectContainer);
+    renderProjects();
+
+    
+    const selectedProject = projects.find(project => project.id === selectedProjectId) || null;
+    if (selectedProject == null) {
+        contentTitle.innerText = 'Home';
+    } 
+    else {
+        contentTitle.innerText = selectedProject.name;
+    }
+}
+
+function renderProjects() {
     projects.forEach(project => {
         const projectElement = document.createElement("li");
         projectElement.dataset.projectId = project.id;
@@ -55,7 +71,7 @@ function render() {
         projectElement.innerText = project.name;
         if (project.id === selectedProjectId) projectElement.classList.add('active-project');
         projectContainer.append(projectElement);
-    })
+    })  
 }
 
 function clearElement(element) {
